abd4112ec98e747e3e93d0cec6cd339a9bc43ce2
Clinty <66639846+ClintyNkolokosa@users.noreply.github.com>
2021-02-14 20:51:40 +0200

    Add files via upload

--- data_cleaning.R
+++ data_cleaning.R
@@ -12,3 +12,18 @@ kasungu_nmcp_2020 <- read.csv(here::here("data", "ku_nmcp_confirmed_cases_2020.c
 View(kasungu_nmcp_2020)
 glimpse(kasungu_nmcp_2020)
 
+
+# Filtering out dry season malaria cases and tidying the data
+# Typically Malawi can be divided into the following seasons:
+#  - a hot and rainy season from November to April 
+#  - and a relatively cool and dry season from May to October
+# Source: https://www.metmalawi.com/climate/climate.php
+
+ku_dry_season_malaria_cases_2020 <- read.csv(here::here("data", "ku_2020_malaria_cases.csv")) %>% 
+                                     dplyr::select(FACILITY_N = `organisationunitname`,
+                                                   May.2020, June.2020, July.2020,
+                                                   August.2020, September.2020, October.2020) %>% 
+                                     dplyr::rowwise() %>% 
+                                     dplyr::mutate(dr_2020 = sum(dplyr::c_across(May.2020:October.2020)))
+ku_2020_malaria_cases
+
